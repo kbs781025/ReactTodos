125faa371e56ee8507c5fb44680013b928b30164
kbs781025 <kbs781025@naver.com>
2020-04-25 23:19:54 +0900

    styled calendar component done

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,4 +1,57 @@
 import React, { useState, useEffect } from "react";
+import styled, { css } from "styled-components";
+
+interface IDateProps {
+  isToday: boolean;
+  isSelected: boolean;
+}
+
+const Frame = styled.div`
+  width: 300px;
+  border: 1px solid lightgrey;
+  box-shadow: 2px 2px 2px #eee;
+`;
+
+const Header = styled.div`
+  font-weight: 500;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Button = styled.button`
+  cursor: pointer;
+`;
+
+const Body = styled.div`
+  width: 100%;
+`;
+
+const Days = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Cell = styled.td<IDateProps>`
+  ${(props) =>
+    props.isToday &&
+    css`
+      border: 1px solid #eee;
+    `}
+
+  ${(props) =>
+    props.isSelected &&
+    css`
+      background-color: #eee;
+    `}
+`;
+
+const Dates = styled.table`
+  width: 100%;
+  td {
+    text-align: center;
+  }
+  cursor: pointer;
+`;
 
 function getStartDay(date: Date) {
   return new Date(date.getFullYear(), date.getMonth(), 1).getDay();
@@ -59,13 +112,13 @@ function Calendar() {
 
   const today = new Date();
   const [date, setDate] = useState(today);
-  const [day, setDay] = useState(today.getDay());
+  const [selectedDate, setselectedDate] = useState(today.getDay());
   const [month, setMonth] = useState(today.getMonth());
   const [year, setYear] = useState(today.getFullYear());
   const [startDay, setStartDay] = useState(getStartDay(today));
 
   useEffect(() => {
-    setDay(date.getDay());
+    setselectedDate(date.getDate());
     setMonth(date.getMonth());
     setYear(date.getFullYear());
     setStartDay(getStartDay(date));
@@ -76,23 +129,47 @@ function Calendar() {
     Array(days[month] + startDay),
     startDay
   );
-  console.log(entireDays);
+
   return (
-    <div>
-      <button onClick={() => setDate(new Date(year, month - 1, day))}>
-        Prev
-      </button>
-      <span>{`${MONTHS[month]} ${year} `}</span>
-      <button onClick={() => setDate(new Date(year, month + 1, day))}>
-        Next
-      </button>
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
-        {DAYS_OF_WEEK.map((day) => {
-          return <strong>{day}</strong>;
-        })}
-      </div>
-      <table>{getRows(entireDays)}</table>
-    </div>
+    <Frame>
+      <Header>
+        <Button
+          onClick={() => setDate(new Date(year, month - 1, today.getDate()))}
+        >
+          Prev
+        </Button>
+        <span>{`${MONTHS[month]} ${year} `}</span>
+        <Button
+          onClick={() => setDate(new Date(year, month + 1, today.getDate()))}
+        >
+          Next
+        </Button>
+      </Header>
+      <Body>
+        <Days>
+          {DAYS_OF_WEEK.map((day) => {
+            return <strong>{day}</strong>;
+          })}
+        </Days>
+        <Dates>
+          {entireDays.map((_dayRow) => (
+            <tr>
+              {_dayRow.map((_day) => (
+                <Cell
+                  onClick={() => {
+                    setselectedDate(_day);
+                  }}
+                  isToday={_day === today.getDate()}
+                  isSelected={_day === selectedDate}
+                >
+                  {_day > 0 ? _day : " "}
+                </Cell>
+              ))}
+            </tr>
+          ))}
+        </Dates>
+      </Body>
+    </Frame>
   );
 }
 
