d24b91de96041801770598e11840017852b19219
kbs781025 <kbs781025@naver.com>
2020-04-27 23:22:50 +0900

    todotype added for convinience

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,20 +1,24 @@
 import React from "react";
 
-interface TodoProps {
+interface TodoType {
   content: string;
   dueDate: Date;
   done: boolean;
+}
+
+interface TodoProps {
+  todo: TodoType;
   onToggle: () => void;
   getDueDate: (date: Date) => string;
 }
 
-function Todo({ content, dueDate, done, onToggle, getDueDate }: TodoProps) {
+function Todo({ todo, onToggle, getDueDate }: TodoProps) {
   return (
     <li>
-      <input type="checkbox" checked={done} onChange={onToggle}></input>
-      <span>{content}</span>
+      <input type="checkbox" checked={todo.done} onChange={onToggle}></input>
+      <span>{todo.content}</span>
       <span> </span>
-      <span>{getDueDate(dueDate)}</span>
+      <span>{getDueDate(todo.dueDate)}</span>
     </li>
   );
 }
